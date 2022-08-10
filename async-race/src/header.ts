b9a25701ac7c43abab3730b656343822dd9de97f
Likade <lera24072@gmail.com>
2022-08-10 15:11:15 +0700

    feat: moving repo

---
+++ async-race/src/header.ts
@@ -0,0 +1,33 @@
+import { MyElement } from './element';
+import './styles/header.css';
+
+export class Header extends MyElement {
+  private nav: MyElement[] = [];
+  private garage: MyElement;
+  private winners: MyElement;
+
+  constructor(parent: HTMLElement) {
+    super(parent, 'div', ['header']);
+    this.garage = new MyElement(this.element, 'a', ['header__button'], 'Garage');
+    this.winners = new MyElement(this.element, 'a', ['header__button'], 'Winners');
+    this.garage.element.setAttribute('href', '#/');
+    this.winners.element.setAttribute('href', '#/winners');
+    this.nav = [this.garage, this.winners];
+    const logo = new MyElement(this.element, 'img', ['header__logo']);
+    logo.element.setAttribute('src', './assets/logo.svg');
+    window.addEventListener('hashchange', () =>
+      this.changeCurrent(this.nav),
+    );
+    window.addEventListener('load', () => this.changeCurrent(this.nav));
+  }
+
+  private changeCurrent(navItems: MyElement[]): void {
+    this.nav = navItems.map((elem) => {
+      elem.element.classList.remove('header__button_active');
+      if (elem.element.getAttribute('href') === window.location.hash) {
+        elem.element.classList.add('header__button_active');
+      }
+      return elem;
+    });
+  }
+}
